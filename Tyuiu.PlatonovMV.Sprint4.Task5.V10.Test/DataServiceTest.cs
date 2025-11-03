54def9a2274e73ce49889486d7a772ec5d2c7bbe
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-11-03 14:58:11 +0500

    Task5

---
+++ Tyuiu.PlatonovMV.Sprint4.Task5.V10.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.PlatonovMV.Sprint4.Task5.V10.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint4.Task5.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            int[,] matrix =
+            {
+                { -3, 4, 0, 2, -1 },
+                { 5, -4, 3, 6, 7 },
+                { -2, 5, 1, -3, 0 },
+                { 4, -1, 6, 2, -4 },
+                { 0, 3, -2, 7, 1 }
+            };
+
+            DataService ds = new DataService();
+            int res = ds.Calculate(matrix);
+
+            Assert.AreEqual(56, res);
+        }
+    }
+}
---
+++ Tyuiu.PlatonovMV.Sprint4.Task5.V10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Tyuiu.PlatonovMV.Sprint4.Task5.V10.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint4.Task5.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Платонов М. В. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                     *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Платонов Максим Владимирович | ИСТНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный массив 5x5, заполненный случайными числами в диапазоне    *");
+            Console.WriteLine("* от -4 до 7. Найти сумму положительных элементов массива.                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+            Random rnd = new Random();
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = rnd.Next(-4, 8);
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            DataService ds = new DataService();
+            int result = ds.Calculate(matrix);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма положительных элементов массива = " + result);
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
