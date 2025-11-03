63abbdfe4ca7dc9901ea63283978eea73ec5c812
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-11-03 13:32:52 +0500

    Task2

---
+++ Tyuiu.PlatonovMV.Sprint4.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.PlatonovMV.Sprint4.Task2.V4.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint4.Task2.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            int[] array = { 2, 3, 9, 6, 5, 8, 7, 4, 9, 2, 5, 3, 7 };
+            DataService ds = new DataService();
+
+            int res = ds.Calculate(array);
+
+            Assert.AreEqual(48, res);
+        }
+    }
+}
---
+++ Tyuiu.PlatonovMV.Sprint4.Task2.V4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Tyuiu.PlatonovMV.Sprint4.Task2.V4.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint4.Task2.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Платонов М. В. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Платонов Максим Владимирович | ИСТНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив из 13 элементов,                    *");
+            Console.WriteLine("* заполненный случайными числами в диапазоне от 2 до 9.                   *");
+            Console.WriteLine("* Найти сумму нечетных элементов массива.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = new int[13];
+            Random rnd = new Random();
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(2, 10);
+                Console.Write(array[i] + " ");
+            }
+
+            DataService ds = new DataService();
+            int result = ds.Calculate(array);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма нечетных элементов массива = " + result);
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
