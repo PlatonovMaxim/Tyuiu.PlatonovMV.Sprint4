2b2fa2655ecf852e0dd52e924cae5f85d0629b40
Максим Платонов <Tyuiu.PlatonovMV@outlook.com>
2025-11-03 14:50:25 +0500

    Task4

---
+++ Tyuiu.PlatonovMV.Sprint4.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.PlatonovMV.Sprint4.Task4.V21.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint4.Task4.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            int[,] matrix =
+            {
+                { 3, 6, 4, 6, 6 },
+                { 7, 5, 5, 5, 8 },
+                { 3, 8, 6, 7, 7 },
+                { 5, 3, 4, 5, 5 },
+                { 7, 4, 3, 7, 8 }
+            };
+
+            DataService ds = new DataService();
+            int res = ds.Calculate(matrix);
+
+            Assert.AreEqual(60, res);
+        }
+    }
+}
---
+++ Tyuiu.PlatonovMV.Sprint4.Task4.V21/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Tyuiu.PlatonovMV.Sprint4.Task4.V21.Lib;
+
+namespace Tyuiu.PlatonovMV.Sprint4.Task4.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Платонов М. В. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнил: Платонов Максим Владимирович | ИСТНб-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный массив 5x5, заполненный числами с клавиатуры в диапазоне  *");
+            Console.WriteLine("* от 3 до 8. Найти сумму чётных элементов массива.                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] matrix = new int[5, 5];
+
+            Console.WriteLine("Введите элементы массива 5x5 (значения от 3 до 8):");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write($"Элемент [{i}, {j}]: ");
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            DataService ds = new DataService();
+            int result = ds.Calculate(matrix);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Сумма чётных элементов массива = " + result);
+            Console.WriteLine();
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
